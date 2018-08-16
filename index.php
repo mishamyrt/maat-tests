a6af19c7e6334ed8e5716918cfa326642b370f2d
mishamyrt <misha@myrt.co>
2018-08-16 12:52:26 +0300

    test

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 require 'vendor/autoload.php';
-
 $maat = new Maat\Renderer();
+$files = glob('tests/*.{txt}', GLOB_BRACE);
+?>
 
-echo $maat->render('**asd**');
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Maat test</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="maat.css">
+  </head>
+  <body>
+    <?php foreach ($files as $file) : ?>
+    <div class="note">
+        <h2 class="title"><?php echo basename($file, ".txt"); ?></h2>
+        <div class="content">
+            <?php echo $maat->render(file_get_contents($file)) ?>
+        </div>
+    </div>
+    <?php endforeach; ?>
+  </body>
+</html>
